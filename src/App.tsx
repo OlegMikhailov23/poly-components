236d72aa2afdd16b017ff97c5a67bbe42a3e6e5f
Oleg Mikhaylov <dkfolkin@gmail.com>
2022-11-26 15:03:24 +0600

    feat: work add types and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Text } from './components/Text';
 
+const Emphasis = ({children} : {children: React.ReactText}) => {
+  return <em style={{backgroundColor: 'yellow', color: 'black', fontSize: '5rem'}}>{children}</em>
+}
+
+
 function App() {
   return (
     <div className="App">
-      <Text as={'h1'}>Hello World</Text>
+      <Text>This is a text node with no as</Text>
+      <Text as={'h1'} color={'green'} style={{backgroundColor: '#000'}}>Hello World</Text>
       <Text as={'h2'}>This is a h2 text</Text>
-      <Text as={'button'}>This is a button text</Text>
+      <Text as={'a'} href={'http://google.com'} target="_blank">This is a anchor text</Text>
+      <Text>This is a text node with no as</Text>
+      <br/>
+
+      <Text as={Emphasis}>This is Emphasis</Text>
     </div>
   );
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from 'react';
-import { echo } from '../../utils/echo';
+import React from 'react';
 
-interface TextProps {
-  as?: any;
-  children: React.ReactNode;
+type Rainbow = 'red' | 'green' | 'yellow' | 'blue' | 'orange' | 'indigo' | 'violet';
+
+type TextProps <C extends React.ElementType> = {
+  as?: C;
+  color?: Rainbow | 'black';  
 }
 
-export const Text: FC<TextProps> = ({ as, children }) => {
+type Props<C extends React.ElementType> = React.PropsWithChildren<
+  TextProps<C>
+> & Omit<React.ComponentPropsWithoutRef<C>, keyof TextProps<C>>
+
+export const Text = <C extends React.ElementType = 'span' >({ 
+  as, 
+  color,
+  children,
+  style,
+  ...props 
+}: Props<C>) => {
   const Component = as || 'span';
-  return <Component>{children}</Component>
+
+  const internalStyles = color ? {style: {...style, color}} : {}
+
+  return <Component { ...props } { ...internalStyles }>{children}</Component>
 }
